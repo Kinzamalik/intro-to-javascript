8c5a5dcb032983b61dc892a5be441ab5fff62790
Kinza Malik <kiinza.malik@gmail.com>
2022-11-07 22:51:48 +0500

    good to go

---
+++ app.js
@@ -0,0 +1,276 @@
+// console.log("Hello world");
+
+//single
+
+/*
+multi line comment
+*/
+
+// window.alert("Allah is one");
+
+// document.write("Kinza")
+
+
+
+
+
+// Variables
+
+// var myName = "Kinza"; //string
+
+// console.log(myName)
+
+// Data types
+
+// String
+
+// var firstName = "Kinza"; 
+// var lastName = 'Malik';
+
+// var myName = firstName + " " + lastName; //concatation
+
+// console.log(myName)
+
+// numbers
+
+// var myAge = 22; 
+
+// var myAgeInLetters = "twenty two";
+
+
+
+
+// var float_num = 10.902;
+
+// console.log(myAge);
+
+// Booleans
+
+// var myTrueStatement = true;
+
+// console.log(myTrueStatement);
+
+// Array
+            //   0     1      2  indexing 
+// var MyData = ['kinza',"malik",22];
+
+// console.log(MyData[0]);
+
+// var myPet = {
+//     'name': 'casper',
+//     'black':'black',
+//     'pet eyes':2
+// }
+
+// console.log(myPet["pet eyes"]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FUNCTIONS
+
+// var myName = "kinza";
+
+// // var myName = "malik";
+
+
+// let myName = 'kinza';
+
+// let myName = 'malik';
+
+
+// console.log(myName);
+
+
+
+// function myUser(value1 , value2){
+
+//     console.log(value1 + value2)
+//     console.log(value1 * value2)
+//     console.log(value1/ value2)
+//     console.log(value1 % value2)
+
+// }
+
+
+// myUser(4, 9);
+
+
+
+
+
+
+
+// booleans
+
+// function bool(){
+//     return true;
+// }
+
+//  console.log(bool());
+
+
+
+
+
+
+
+// var operators = "kinza";
+
+
+
+
+
+
+// var cupcakes = 10;
+// var result;
+
+// if(cupcakes > 0){
+//     result = "I m Happy";
+// }else{
+//     result = "sad :(";
+// }
+
+// console.log(result);
+
+
+
+
+
+
+
+// let day;
+
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = "sunday";
+//     break;
+
+//   case 1:
+//     day = "Monday";
+//     break;
+
+//   case 2:
+//     day = "Tuesday";
+//     break;
+
+//   case 3:
+//     day = "Wednesday";
+//     break;
+// }
+
+// console.log(day)
+
+
+
+// for(let i = 0; i < 10; i++){
+//     console.log("Hello world");
+// }
+
+
+
+// var  x = 4;
+
+// while( x == 4){
+// console.log("running")
+// }
+
+
+
+// document.getElementById("my_btn").addEventListener("click", displayDate);
+
+// function displayDate() {
+//     document.getElementById("demo").innerHTML = Date();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
